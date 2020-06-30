91287654844f98352f3875bd90af553d453089d7
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-06-30 14:23:03 -0300

    Explorando manipulação de props, e criando métodos de incrementar e decrementar entre components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 
+import Counter from './Counter';
 
-const store = [{
-  type: "Roupa"
-},
-{
-  type: "Calçado"
-},
-{
-  type: "Camistea"
-}];
+const Buttons = ({ increment, decrement, count }) => (
+  <div>
+    <h1>Valor Atual {count} </h1>
 
-function Column({ type }) {
-  return (
-    <tr>
-      <td>{type}</td>
-    </tr>
-  )
+    <div>
+      <button onClick={increment}> Adicionar 1 </button>
+    </div>
+    <div>
+      <button onClick={decrement}> Remover 1 </button>
+    </div>
+
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter render={({increment, decrement, count}) => (
+          <Buttons 
+            increment={increment}
+            decrement={decrement}
+            count={count}
+          />
+        )}>
+
+        </Counter>
+      </div>
+    )
+  }
 }
 
-export default function App() {
-
-  const renderColumns = (element, key) => (
-    <Fragment key={`column-${key}`}>
-      <Column type={element.type}/>
-    </Fragment>
-  );
-
-  return (
-    <table>
-      {store.map(renderColumns)}
-    </table>
-  )
-}
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
 const main = (
